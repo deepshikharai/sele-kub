bf58402b766eb876df5b624aad7f5e6a8b13fd1f
deepshikharai <37527448+deepshikharai@users.noreply.github.com>
2018-10-15 14:24:03 +0530

    Update SimpleKubSeleniumGridTest.java

--- sele-kub/src/main/java/demojar/SimpleKubSeleniumGridTest.java
+++ sele-kub/src/main/java/demojar/SimpleKubSeleniumGridTest.java
@@ -37,15 +37,15 @@ public class SimpleKubSeleniumGridTest {
 			System.out.println("Exception is=="+e.getMessage());
 		}
 		DesiredCapabilities dcp = new DesiredCapabilities();
-		dcp.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
-		dcp.setCapability(CapabilityType.PLATFORM, Platform.LINUX);
+		dcp.setCapability(CapabilityType.BROWSER_NAME, BrowserType.IE);
+		dcp.setCapability(CapabilityType.PLATFORM, Platform.WINDOWS);
 		dcp.setCapability(CapabilityType.SUPPORTS_APPLICATION_CACHE, true);
 		dcp.setCapability(CapabilityType.SUPPORTS_NETWORK_CONNECTION, true);
 		dcp.setCapability("name", "KubernetesGridTest");
 		dcp.setCapability("idleTimeout", 150);
 		//driver = new RemoteWebDriver(new URL("http://35.193.7.170:4444/wd/hub"),dcp);
 		//System.out.println(pro.getProperty("selenium.url"));
-		driver = new RemoteWebDriver(new URL(pro.getProperty("selenium.url")),dcp);
+		driver = new RemoteWebDriver(new URL("http://35.185.83.18:4444/wd/hub"),dcp);
 		//System.setProperty("webdriver.chrome.driver","D:\\driver\\chromedriver_win32\\chromedriver.exe");
 		/*System.setProperty("webdriver.chrome.driver",pro.getProperty("chrome.driver").toString());
 		//Open browser instance
